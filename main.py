221b76cb5213e653bf59bfe559aeca472b669c97
igor98santos <igmascaras58@gmail.com>
2022-05-21 21:02:48 -0300

    Função Map em python

--- main.py
+++ main.py
@@ -76,8 +76,19 @@ print("Olá mundo!")
 #media = lambda n1,n2,n3,n4 :  (n1+n2+n3+n4)/ 4
 #print(media(7, 3, 5, 8))
 
-
-
+#Função map
+#*
+# kmh = [40, 50, 56, 64, 73, 79, 85, 96, 100, 120]
+#
+# mph = []
+#
+# for i in kmh:
+#     mph.append(i/1.61)
+# print(mph)
+#
+# mph2 = list(map(lambda  x: x/1.61, kmh))
+#
+# print(mph2)
 
 
 
